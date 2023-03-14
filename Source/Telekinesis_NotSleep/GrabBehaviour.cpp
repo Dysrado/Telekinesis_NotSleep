0184ee23ffc5275f0a4453ba0c9d7d154f44c755
Dysrado <rajjardin@gmail.com>
2023-03-14 13:43:23 +0800

    Pushed Grab Behavior
    Note the ff:
    There is no release key yet
    offset does not work (check tickcomponent, settargetlocation

---
+++ Source/Telekinesis_NotSleep/GrabBehaviour.cpp
@@ -0,0 +1,112 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GrabBehaviour.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+#include "Engine/SkeletalMeshSocket.h"
+
+
+#define out
+// Sets default values for this component's properties
+UGrabBehaviour::UGrabBehaviour()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabBehaviour::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	
+	// ...
+	UInputComponent* inputComponent = this->GetOwner()->FindComponentByClass<UInputComponent>();
+	if (inputComponent != nullptr) {
+		inputComponent->BindAction("Grab", EInputEvent::IE_Pressed, this, &UGrabBehaviour::Grab);
+		inputComponent->BindAction("Release", EInputEvent::IE_Pressed, this, &UGrabBehaviour::Release);
+		inputComponent->BindAction("Throw", EInputEvent::IE_Pressed, this, &UGrabBehaviour::Throw);
+	}
+	
+}
+
+
+// Called every frame
+void UGrabBehaviour::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	;FName name = "Offset Socket";
+	// ...
+	if (hasGrabbed) {
+		FVector location;
+		FRotator rotation;
+
+		GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(out location, out rotation);
+		FVector lineTracedEnd = location + rotation.Vector() * REACH;
+
+		if (grabbedActor != nullptr) {
+
+			if (physicsHandle != nullptr) {
+				physicsHandle->SetTargetLocation();
+			}
+			else {
+				FVector grabbedLoc = grabbedActor->GetActorLocation();
+				grabbedLoc = lineTracedEnd;
+				grabbedActor->SetActorLocation(grabbedLoc);
+			}
+		}
+	}
+}
+
+void UGrabBehaviour::Grab()
+{
+	FVector location;
+	FRotator rotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(out location, out rotation);
+	FVector lineTracedEnd = location + rotation.Vector() * REACH;
+	FVector lineTracedLoc = location + rotation.Vector() * REACH;
+
+	DrawDebugLine(GetWorld(), location, lineTracedEnd, FColor::Blue, false, 1.0f, 0, 5.0f);
+
+	FHitResult hit;
+	FCollisionQueryParams traceParams(FName(TEXT("traceQuery")), false, GetOwner()); // parameters for the collisions
+	FCollisionObjectQueryParams objectTypeParams(ECollisionChannel::ECC_PhysicsBody); // Objects you want to get hit, ECC_PhysicsBody = all physics objects
+	GetWorld()->LineTraceSingleByObjectType(hit, location, lineTracedEnd, objectTypeParams, traceParams);
+
+	grabbedActor = hit.GetActor();
+	if (grabbedActor != nullptr) {
+		hasGrabbed = true;
+		primitiveComp = hit.GetComponent();
+		//UE_LOG(LogTemp, Warning, TEXT("Grabbed %s"), *grabbedActor->GetName());
+
+		physicsHandle = grabbedActor->FindComponentByClass<UPhysicsHandleComponent>();
+		if (physicsHandle != nullptr) {
+			physicsHandle->GrabComponentAtLocation(primitiveComp, EName::None, lineTracedEnd);
+		}
+	
+	}
+
+}
+
+void UGrabBehaviour::Release()
+{
+	hasGrabbed = false;
+	grabbedActor = nullptr;
+
+	if (physicsHandle != nullptr) {
+		physicsHandle->ReleaseComponent();
+	}
+}
+
+
+void UGrabBehaviour::Throw()
+{
+	
+}
+
---
+++ Source/Telekinesis_NotSleep/GrabBehaviour.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "GrabBehaviour.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TELEKINESIS_NOTSLEEP_API UGrabBehaviour : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabBehaviour();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+	UPROPERTY(EditAnywhere) float REACH = 600;
+	AActor* grabbedActor = NULL;
+	bool hasGrabbed = false;
+	UPrimitiveComponent* primitiveComp;
+	UPhysicsHandleComponent* physicsHandle = NULL;
+	void Grab();
+	void Release();
+	void Throw();
+
+	//UPROPERTY(EditAnywhere) AActor* Offset;
+	USkeletalMesh* mesh;
+	UPROPERTY(EditAnywhere) AActor* offset;
+		
+};
